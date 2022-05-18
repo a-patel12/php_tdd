8d66cc10c2b515f026def6c29310cd14587106e8
a patel <5598313+a-patel12@users.noreply.github.com>
2022-05-18 07:38:09 -0400

    exception handling and one invalid test case added

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,34 +1,36 @@
 <?php
 namespace Apatel\PhpTdd;
+use PHPUnit\Exception;
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase{
 
     private $calculator;
 
-    protected function setUp(): void
-    {
+    protected function setUp(): void{
         $this->calculator = new Calculator();
     }
 
     public function addDataProvider() {
-
         return [
             [1,2,3],
             [0,0,0],
             [-1,-1,-2],
+           // ['a',1,2], // this will trigger exception
         ];
-
     }
 
     /**
      * @dataProvider addDataProvider
      */
-    public function testAdd($a, $b, $expected)
-    {
-        $result = $this->calculator->add($a, $b);
-        $this->assertEquals($expected, $result);
-
+    public function testAdd($a, $b, $expected){
+        try{
+            $result = $this->calculator->add($a, $b);
+            $this->assertEquals($expected, $result);
+        }
+        catch (Exception $exception){
+            $this->fail('Message: ' .$exception->getMessage());
+        }
     }
 
 }
